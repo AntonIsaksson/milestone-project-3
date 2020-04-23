9b781f8de861c8f2df8bfa4bcaa75be5a0a9568f
AntonIsaksson <anton.isak@outlook.com>
2020-04-23 10:42:54 +0200

    Added insert and get category

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
+
+
 # Route to homepage
 @app.route('/')
 @app.route('/home')
@@ -32,7 +34,37 @@ def show_categories():
 @app.route('/lists')
 def show_lists():
     return render_template("lists.html", 
-                            categories=list(mongo.db.categories.find()))
+                            categories=list(mongo.db.categories.find()),
+                            lists=list(mongo.db.lists.find()),
+                            list_object=list(mongo.db.list_object.find()))
+
+
+@app.route('/create_list')
+def create_list():
+    return render_template("create_list.html", 
+                            categories=list(mongo.db.categories.find()),
+                            lists=list(mongo.db.lists.find()),
+                            list_object=list(mongo.db.list_object.find()))
+
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html',
+    categories=list(mongo.db.categories.find()))
+
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
+    return redirect(url_for('create_list'))  
+
+
+@app.route('/add_category')
+def add_category():
+    return render_template('add_category.html')                          
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+
+    $(".modal-open").click(function () {
+        $("#list-item-modal").modal('show');
+    });
+  });
