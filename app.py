09d95dd27d74cec1d1e6283721cace77030cf71c
AntonIsaksson <anton.isak@outlook.com>
2020-04-24 09:34:38 +0200

    Took away Categories, Simplified page Structure

--- app.py
+++ app.py
@@ -25,44 +25,29 @@ def home_page():
     return render_template ("base.html")
 
 
-@app.route('/show_categories')
-def show_categories():
-    return render_template("categories.html", 
-                            categories=list(mongo.db.categories.find()))
-
-
 @app.route('/lists')
 def show_lists():
-    return render_template("lists.html", 
-                            categories=list(mongo.db.categories.find()),
-                            lists=list(mongo.db.lists.find()),
-                            list_object=list(mongo.db.list_object.find()))
+    return render_template("lists.html",
+                            lists=list(mongo.db.lists.find()))
+
+
+@app.route('/show_list')
+def show_list():
+    return render_template("show_list.html", 
+                            lists=list(mongo.db.lists.find()))
 
 
 @app.route('/create_list')
 def create_list():
-    return render_template("create_list.html", 
-                            categories=list(mongo.db.categories.find()),
-                            lists=list(mongo.db.lists.find()),
-                            list_object=list(mongo.db.list_object.find()))
+    return render_template("create_list.html",
+                            lists=list(mongo.db.lists.find()))
 
 
-@app.route('/get_categories')
-def get_categories():
-    return render_template('categories.html',
-    categories=list(mongo.db.categories.find()))
 
-
-@app.route('/insert_category', methods=['POST'])
-def insert_category():
-    category_doc = {'category_name': request.form.get('category_name')}
-    mongo.db.categories.insert_one(category_doc)
-    return redirect(url_for('create_list'))  
-
-
-@app.route('/add_category')
-def add_category():
-    return render_template('add_category.html')                          
+@app.route('/insert_list_name', methods=['POST'])
+def insert_list_name():
+    list_name = {'list_name': request.form.get('list_name')}
+    mongo.db.lists.insert_one(list_name)                       
 
 
 
