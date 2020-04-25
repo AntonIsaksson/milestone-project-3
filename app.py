2e4eff7bad03e1689d8ff1e2011eb82ec6e4828f
AntonIsaksson <anton.isak@outlook.com>
2020-04-25 17:33:44 +0200

    Updates for Heroku Deployment

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.secret_key = os.getenv("SECRET", "mySecretKey")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 app.config["MONGO_DBNAME"] = "topTenDB"
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
