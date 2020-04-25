2b90b4dd04e4bdc1d836b100c618b21c33876599
AntonIsaksson <anton.isak@outlook.com>
2020-04-25 13:43:05 +0200

    Added User interface with Login, Registration and Logout

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
+import bcrypt
 import pymongo
 from pymongo import MongoClient
 from flask_pymongo import PyMongo
@@ -17,12 +18,70 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 
+"""def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+    return render_template('index.html')"""
+
+@app.route('/')
+
 
 # Route to homepage
-@app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template ("base.html")
+    if 'username' in session:
+        return render_template("logged-in-home.html", users = mongo.db.users.find_one({'name': session['username']}))
+    return render_template ("home.html")
+
+
+@app.route('/to_login')
+def to_login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('home_page'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/logout')
+def logout():
+    """Logs the user out and redirects to home"""
+    session.clear()  # Kill session
+    return redirect(url_for('home_page'))
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+
+@app.route('/account')
+def account():
+    return render_template('account.html', users = mongo.db.users.find_one({'name': session['username']}))
+
+
+
+
 
 
 @app.route('/lists')
@@ -31,10 +90,10 @@ def show_lists():
                             lists=list(mongo.db.lists.find()))
 
 
-@app.route('/show_list')
-def show_list():
+@app.route('/show_list/<lists_id>')
+def show_list(lists_id):
     return render_template("show_list.html", 
-                            lists=list(mongo.db.lists.find()))
+                            lists=mongo.db.lists.find_one({"_id": ObjectId(lists_id)}))
 
 
 @app.route('/create_list')
