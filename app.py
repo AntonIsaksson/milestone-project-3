804e997546b85849434744213e844843b3436b2f
AntonIsaksson <anton.isak@outlook.com>
2020-04-26 14:53:32 +0200

    Fixed show_list so relevant content reviels when clicked in show_list

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, session
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 import bcrypt
+import re
 import pymongo
 from pymongo import MongoClient
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, DESCENDING
 from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"):
@@ -12,6 +13,7 @@ if path.exists("env.py"):
 app = Flask(__name__)
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+app.secret_key = os.environ.get('secret_key')
 app.config["MONGO_DBNAME"] = "topTenDB"
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
@@ -19,8 +21,6 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-
-
 # Route to homepage
 @app.route('/home')
 def home_page():
@@ -43,7 +43,6 @@ def login():
         if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
             return redirect(url_for('home_page'))
-
     return 'Invalid username/password combination'
 
 @app.route('/logout')
@@ -76,36 +75,57 @@ def account():
 
 
 
-
-
-
-@app.route('/lists')
-def show_lists():
-    return render_template("lists.html",
-                            lists=list(mongo.db.lists.find()))
-
-
-@app.route('/show_list')
+@app.route('/show_list')#=get_recipes
 def show_list():
-    lists = mongo.db.lists.find()
-    return render_template("show_list.html", lists=lists)
+    return render_template("lists.html", lists = mongo.db.lists.find())
+
+
+@app.route('/view_lists/<list_id>')#=view_recipe
+def view_lists(list_id):
+    lists = mongo.db.lists
+    list_db=lists.find_one({'_id': ObjectId(list_id)})
+    return render_template("show_list.html", lists=list_db, listName = mongo.db.lists.find())
+
 
 
 @app.route('/create_list')
 def create_list():
-    return render_template("create_list.html",
-                            lists=list(mongo.db.lists.find()))
+    return render_template("create_list.html")
 
 
 
-@app.route('/insert_list_name', methods=['POST'])
-def insert_list_name():
-    list_name = {'list_name': request.form.get('list_name')}
-    mongo.db.lists.insert_one(list_name)                       
+#lists.insert_one(request.form.to_dict())
+@app.route('/insert_list', methods=['POST'])
+def insert_list():
+    lists = mongo.db.lists
+    if request.method == "POST":
+        form = request.form.to_dict()
+        myListObject = [
+            {
+            "list_object_number": form['list_object_number_1'],
+            "list_object_name": form['list_object_name_1'],
+            "list_object_description": form['list_object_description_1']         
+            },
+            {
+            "list_object_number": form['list_object_number_2'],
+            "list_object_name": form['list_object_name_2'],
+            "list_object_description": form['list_object_description_2']         
+            }
+        ]
+        
+        obj = {
+            "list_name": form['list_name'],
+            "list_objects": myListObject,
+            "user": session['username']
 
+        }
+        lists.insert_one(obj)
+    
+    return redirect(url_for('home_page'))                     
 
 
 if __name__ == "__main__":
+    #app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=False)
+        port=os.environ.get('PORT'),
+        debug=True)
--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["MONGO_URI"]="mongodb+srv://root:r00t@myfirstcluster-ab99d.mongodb.net/topTenDB?retryWrites=true&w=majority"
 os.environ["SECRET_KEY"] = "ohsosecret"
+os.environ["secret_key"] = "mysecretkey"
